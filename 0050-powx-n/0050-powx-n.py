d8a39a35f9e477324859873c53c8c624e7a28b18
Rajesh650 <54935530+Rajesh650@users.noreply.github.com>
2023-03-29 05:40:42 +0530

    Time: 36 ms (34.61%), Space: 13.8 MB (53.25%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,4 +1,12 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return pow(x,n)
+        
+        if x==0:
+            return 0
+        elif n ==0:
+            return 1 
+        elif (x==0 )and  (n==0):
+            return 1
+        else :
+            return pow(x,n)
         
